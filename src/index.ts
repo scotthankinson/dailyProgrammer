78b7e024bb0909b940c48e43179f534509b3f675
Scott Hankinson <scott.hankinson@slalom.com>
2018-02-21 13:35:22 -0600

    BigNumber fixer upper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 "use strict";
 // tslint:disable
+import { BigNumber } from 'bignumber.js';
 
 const start_feb_20 = (): void => {
     solve_feb_20(`15674
@@ -18,18 +19,17 @@ const solve_feb_20 = (input: string) => {
     console.log(input);
     const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     console.log(parseInt(input));
-    // TODO: fails for parseInt on large integers because javascript
-    const inputs: number[] = input.split("\n").map(value => parseInt(value.trim(), 10));
-    console.log(inputs);
+    const inputs: BigNumber[] = input.split("\n").map(value => new BigNumber(value.trim(), 10));
+    // console.log(inputs);
     inputs.forEach(value => console.log(encodeBase62(value, alphabet)));
 }
 
-const encodeBase62 = (input: number, alphabet: string[]) => {
+const encodeBase62 = (input: BigNumber, alphabet: string[]) => {
     let output = "";
-    while (input > 0) {
-        const digit = input % 62;
-        output = output + alphabet[digit];
-        input = Math.trunc(input / 62);
+    while (input.comparedTo(0) > 0) {
+        const digit = input.modulo(62);
+        output = output + alphabet[digit.toNumber()];
+        input = input.dividedToIntegerBy(62);
     }
     return output;
 }
